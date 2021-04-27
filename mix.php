951f36adcbaaee7061e756e988910899697b9ef8
Lukas Gaechter <lukas.gaechter@mind.ch>
2021-04-27 22:13:07 +0200

    Add is_child argument

--- mix.php
+++ mix.php
@@ -12,44 +12,62 @@
  * @since 1.0.0
  *
  * @param string $path The relative path to the file.
- * @param string $manifest_directory Optional. Custom path to manifest directory. Default 'build'.
+ * @param string $manifest_directory Optional. Custom relative path to manifest directory. Default 'build'.
  *
  * @return string The versioned file URL.
  */
-function mix( $path, $manifest_directory = 'build' ) {
-	static $manifest;
-	static $manifest_path;
+function mix( $path, $manifest_directory = 'build', $args = [] ) {
+	// Manifest content cache.
+	static $manifests = [];
 
-	if ( ! $manifest_path ) {
-		$manifest_path = get_theme_file_path( $manifest_directory . '/mix-manifest.json' );
+	$args = wp_parse_args( $args, [
+		'is_child' => false,
+	] );
+
+	if ( $args['is_child'] ) {
+		$base_path = trailingslashit( get_theme_file_path() );
+	} else {
+		$base_path = trailingslashit( get_parent_theme_file_path() );
 	}
 
-	// Bailout if manifest couldn’t be found
+	$manifest_path = $base_path . trailingslashit( $manifest_directory ) . 'mix-manifest.json';
+
+	// Bailout if manifest couldn’t be found.
 	if ( ! file_exists( $manifest_path ) ) {
-		return get_theme_file_uri( $path );
+		return $base_path . $path;
 	}
 
-	if ( ! $manifest ) {
+	if ( ! isset( $manifests[ $manifest_path ] ) ) {
 		// @codingStandardsIgnoreLine
-		$manifest = json_decode( file_get_contents( $manifest_path ), true );
+		$manifests[ $manifest_path ] = json_decode( file_get_contents( $manifest_path ), true );
 	}
 
-	// Remove manifest directory from path
+	$manifest = $manifests[ $manifest_path ];
+
+	// Remove manifest directory from path.
 	$path = str_replace( $manifest_directory, '', $path );
-	// Make sure there’s a leading slash
+	// Make sure there’s a leading slash.
 	$path = '/' . ltrim( $path, '/' );
 
-	// Bailout with default theme path if file could not be found in manifest
+	// Bailout with default theme path if file could not be found in manifest.
 	if ( ! array_key_exists( $path, $manifest ) ) {
-		return get_theme_file_uri( $path );
+		if ( $args['is_child'] ) {
+			return get_theme_file_uri( $path );
+		}
+
+		return get_parent_theme_file_uri( $path );
 	}
 
-	// Get file URL from manifest file
+	// Get file URL from manifest file.
 	$path = $manifest[ $path ];
-	// Make sure there’s no leading slash
+	// Make sure there’s no leading slash.
 	$path = ltrim( $path, '/' );
 
-	return get_theme_file_uri( trailingslashit( $manifest_directory ) . $path );
+	if ( $args['is_child'] ) {
+		return get_theme_file_uri() . '/' . trailingslashit( $manifest_directory ) . $path;
+	}
+
+	return get_parent_theme_file_uri( trailingslashit( $manifest_directory ) . $path );
 }
 
 /**
